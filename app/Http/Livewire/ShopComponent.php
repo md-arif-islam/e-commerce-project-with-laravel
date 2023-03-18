952938cf81799cc6733ce40b8fd77a8c8a85e7f4
md-arif-islam <arifislamarif344@gmail.com>
2023-03-18 16:58:51 +0600

    Dynamic Products - Shop Page

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\WithPagination;
 
-class ShopComponent extends Component
-{
-    public function render()
-    {
-        return view('livewire.shop-component');
+class ShopComponent extends Component {
+    use WithPagination;
+
+    public function render() {
+
+        $products = Product::paginate( 10 );
+        return view( 'livewire.shop-component', ['products' => $products] );
+
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,19 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    public function run() {
+        // Category::factory( 6 )->create();
+        Product::factory( 16 )->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
