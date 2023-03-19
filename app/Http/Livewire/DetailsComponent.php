f05b7d85ce4170670a63c449df5dcf6eb4b5331f
md-arif-islam <arifislamarif344@gmail.com>
2023-03-19 11:12:47 +0600

    Make related and new products --dynamic

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -13,7 +13,12 @@ class DetailsComponent extends Component {
     }
 
     public function render() {
+
         $product = Product::where( 'slug', $this->slug )->first();
-        return view( 'livewire.details-component', ['product' => $product] );
+        $related_products = Product::where( 'category_id', $product->category_id )->inRandomOrder()->limit( 4 )->get();
+        $new_products = Product::latest()->limit( 4 )->get();
+
+        return view( 'livewire.details-component', ['product' => $product, 'related_products' => $related_products, 'new_products' => $new_products] );
+
     }
 }
