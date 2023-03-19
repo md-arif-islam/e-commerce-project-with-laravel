c82b4eca96eae8e4991b90884b29b4eb3885739a
md-arif-islam <arifislamarif344@gmail.com>
2023-03-19 10:52:40 +0600

    Product Details Page

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
@@ -22,6 +23,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get( '/', HomeComponent::class )->name( "home.index" );
 Route::get( '/shop', ShopComponent::class )->name( "shop" );
+Route::get( '/product/{slug}', DetailsComponent::class )->name( "product.details" );
 Route::get( '/cart', CartComponent::class )->name( "shop.cart" );
 Route::get( '/checkout', CheckoutComponent::class )->name( "shop.checkout" );
 
