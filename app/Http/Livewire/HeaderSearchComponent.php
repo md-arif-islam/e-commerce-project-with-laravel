5b17cd7a3b33e6a0f064d15c5d4ad548140f1701
md-arif-islam <arifislamarif344@gmail.com>
2023-03-28 10:13:58 +0600

    refactor(search-bar) update search bar logic for better usability

--- app/Http/Livewire/HeaderSearchComponent.php
+++ app/Http/Livewire/HeaderSearchComponent.php
@@ -4,10 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class HeaderSearchComponent extends Component
-{
-    public function render()
-    {
-        return view('livewire.header-search-component');
+class HeaderSearchComponent extends Component {
+
+    public $q;
+
+    public function mount() {
+        $this->fill( request()->only( 'q' ) );
+    }
+
+    public function render() {
+        return view( 'livewire.header-search-component' );
     }
 }
